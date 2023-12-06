d732491dd497165d468cda1dfd99a6900b20d63f
foglet15 <emdort@gmail.com>
2023-12-06 10:48:27 -0500

    day 5 part 2

--- 2023/day_five_seed/src/main.rs
+++ 2023/day_five_seed/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, collections::VecDeque};
 
 fn main() {
-    let file = "example_input.txt";
+    let file = "input.txt";
     let contents = fs::read_to_string(file).expect("couldn't open input file");
     let mut lines = contents.lines();
     // parse seeds
@@ -40,34 +40,40 @@ fn main() {
     let seeds: Vec<_> = seeds.into_iter().map(|seed| seed_translator(seed, &parsed_maps)).collect();
     println!("translated seeds are {:?}", seeds.clone());
     println!("seed min is {}", seeds.clone().into_iter().min().unwrap());
-    // part 2 create ranges
+    println!();
+    println!();
+    // part 2 iterate translation of each range
     let seeds: Vec<_> = nums_line_to_vec(seed_line.clone());
     let even_seeds = seeds.clone().into_iter().step_by(2);
     let odd_seeds = seeds.clone().into_iter().skip(1).step_by(2);
-    let seed_ranges = even_seeds.zip(odd_seeds).collect::<Vec<_>>();
-
-    let d = parsed_maps.iter().map(|v| v.iter().map(|i| i.iter().nth(1).unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
-    println!("d is {:?}", d.clone());
-    //let mut dq: VecDeque<_> = VecDeque::from(d);
-    let mut tr_pts = Vec::new();
-    for (idx, pt) in d.clone().iter().enumerate() {
-        let map = parsed_maps.clone().into_iter().skip(idx+1).collect::<Vec<Vec<_>>>();
-        if map.len() != 0 {
-            for p in pt {
-                tr_pts.push(seed_translator(**p, &map))
-            }
-        }
-        }
-    println!("tr_pts is {:?}", tr_pts);
-
+    let seed_ranges = even_seeds.clone().zip(odd_seeds).collect::<Vec<_>>();
+    let mut points: Vec<u64> = even_seeds.collect::<Vec<_>>();
+    // iteratively step through getting the lowest start of each range at each map
+    // the end of this is the start of all final ranges, which is the lowest
+    for idx in 0..parsed_maps.len() {
+        let map = parsed_maps.clone().into_iter().skip(idx).take(1).collect::<Vec<_>>();
+        let mut translated = points.iter().map(|p| seed_translator(*p, &map)).collect::<Vec<_>>();
+        translated.extend(map.first().unwrap().iter().map(|r| r[0]).collect::<Vec<_>>());
+        points = translated;
+    }
+    println!("pts is {:?}", points);
+    // work back from the output ranges to all possible inputs based on final ranges
+    let points = points.iter().map(|p| seed_translator_reversed(p.clone(), &parsed_maps)).collect::<Vec<_>>(); 
+    // filter the possible to only the valid based on the input ranges
+    let points = points.iter().filter(|p| seed_ranges.iter().any(|r| r.0 <= **p && **p < r.0+r.1)).collect::<Vec<_>>();
+    println!("pts is {:?}", points);
+    // translate the filtered input ranges and get the min
+    let points = points.iter().map(|p| seed_translator(**p, &parsed_maps)).collect::<Vec<_>>();
+    println!("pts is {:?}", points);
+    println!("min is {}", points.iter().min().unwrap());
 }
 
 fn seed_translator_reversed(mut seed: u64, parsed_maps: &Vec<Vec<Vec<u64>>>) -> u64 {
     for map in parsed_maps.into_iter().rev() {
         let init_seed = seed.clone();
         for range in map.into_iter().rev() {
-            if (seed >= range[1] && seed < range[1]+range[2]) {
-                seed = (seed - range[1]) + range[0];
+            if (seed >= range[0] && seed < range[0]+range[2]) {
+                seed = (seed - range[0]) + range[1];
                 break;
             }
             else {
@@ -80,7 +86,6 @@ fn seed_translator_reversed(mut seed: u64, parsed_maps: &Vec<Vec<Vec<u64>>>) -> 
 
 fn seed_translator(mut seed: u64, parsed_maps: &Vec<Vec<Vec<u64>>>) -> u64 {
     for map in parsed_maps {
-        let init_seed = seed.clone();
         for range in map {
             if (seed >= range[1] && seed < range[1]+range[2]) {
                 seed = (seed - range[1]) + range[0];
